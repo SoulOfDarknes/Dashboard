564a74c0ba93e556ccfd1f7deebbcc590101c358
SoulOfDarknes <ve4nuy@rambler.ru>
2023-02-27 02:47:58 +0200

    some ideas

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,69 @@
-const { readFileSync } = require('fs');
-const { compileFile } = require('pug');
+const fs = require('fs');
+const pug = require('pug');
 
-const compileFunc = compileFile('../modules/table.pug');
+let compileFile = pug.compileFile('../modules/table.pug')
+users = JSON.parse(fs.readFileSync('./accounts.json'));
+console.log(compileFile(users));
 
-const users = JSON.parse(readFileSync('./accounts.json'));
 
-const tableHtml = compileFunc({ users });
-console.log(users)
-// window.querySelector('.content__table').innerHTML = tableHtml;
+// let html = compileFile(users);
+// fs.writeFile(html)
+// pug.renderFile('../modules/table.pug', { users: users });
 
+// document.querySelector('.content__inner').innerHTML = tableHtml;
+
+// var pug = require('pug');
+// var fn = pug.compileFile('../modules/table.pug');
+// var locals = require('./accounts.json');
+// console.log(fn({
+//     users: locals
+// }));
+// console.log(locals)
+
+
+
+// - its works on Node.js local server
+// const express = require('express');
+// const app = express();
+// const pug = require('pug');
+// const { readFileSync } = require('fs');
+// const path = require('path');
+
+
+// const users = JSON.parse(readFileSync('./accounts.json'));
+// console.log(users)
+
+// app.use(express.static('/src'));
+
+// app.get('/', (req, res) => {
+//     const html = pug.renderFile('../index.pug', { users: users });
+//     res.send(html);
+// });
+
+
+
+// app.get('/style/dist/output.css', function (req, res) {
+//     res.setHeader('Content-Type', 'text/css');
+//     res.sendFile(path.join(__dirname, '../style/dist/output.css'));
+// });
+
+// const fs = require('fs');
+// const postcss = require('postcss');
+// const postcssImport = require('postcss-import');
+
+// const css = fs.readFileSync('../style/dist/style.css', 'utf8');
+
+// postcss()
+//     .use(postcssImport())
+//     .process(css, { from: '../style/dist/style.css', to: '../style/dist/style.css' })
+//     .then(result => {
+//         fs.writeFileSync('../style/dist/output.css', result.css);
+//     });
+
+
+// app.listen(3000, () => {
+//     console.log('Server started on port 3000');
+// });
 
 
 
